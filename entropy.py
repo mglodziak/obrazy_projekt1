47bd3238ecb561120f97edb677dad06417883886
Miki <Miki@localhost.localdomain>
2020-05-27 15:04:22 +0200

    add entropy trailer

---
+++ entropy.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def normalize_img(img):
+
+def entropy(img):
+    mask=input("Entry size of square mask (for example 6): ")
+    width=img.shape[0]
+    height=img.shape[1]
+    for i in range(height):
+        for j in range(width):
+            
